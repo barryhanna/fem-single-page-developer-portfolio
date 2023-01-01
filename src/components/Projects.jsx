383844a7120a3fec03cc940829892c64f4d1cef0
Barry Hanna <barryahanna@gmail.com>
2023-01-01 19:03:07 +0000

    Button style

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import Container from './Container'
+import React from 'react';
+import Container from './Container';
 
 export default function Projects() {
-	return (
-		<div>
-			<Container>
-				<div className="projects-header">
-					<h2>Projects</h2>
-					<button>Contact Me</button>
-				</div>
-				<div className="projects-list">
-					<div className="project-item">
-						<img src="#" alt="" />
-						<h4 className="project-title">Design Portfolio</h4>
-						<div className="skills">
-							<p><span>HTML</span><span>CSS</span></p>
-						</div>
-					</div>
-				</div>
-			</Container>
-		</div>
-	)
+  return (
+    <div>
+      <Container>
+        <div className="projects-header">
+          <h2>Projects</h2>
+          <button>Contact Me</button>
+        </div>
+        <div className="projects-list">
+          <div className="project-item">
+            <img src="../assets/images/thumbnail-project-1-small.webp" alt="" />
+            <h4 className="project-title">Design Portfolio</h4>
+            <div className="skills">
+              <p>
+                <span>HTML</span>
+                <span>CSS</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
