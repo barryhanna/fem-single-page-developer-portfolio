b7e62b3a9201552b929286931805b51aede808d8
Barry Hanna <barryahanna@gmail.com>
2023-02-11 19:43:01 +0000

    FIX: border-color on social-links

--- src/components/icons/LinkedInIcon.jsx
+++ src/components/icons/LinkedInIcon.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function FrontendMentorIcon() {
+export default function LinkedInIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24">
       <path
