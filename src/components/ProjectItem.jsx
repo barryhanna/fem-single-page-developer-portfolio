8d8d1c4a8627c5b210197effbc1d3291ca861786
Barry Hanna <barryahanna@gmail.com>
2023-02-12 20:43:42 +0000

    Hover overlay for project item screenshot

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-// name: 'Design Portfolio',
-//     skills: ['HTML', 'CSS'],
-//     screenshot: '/assets/images/thumbnail-project-1-large.webp',
-//     link: 'http://website.com',
-//     src: 'https://github.com',
-
 export default function ProjectItem({
   name = 'Project',
   skills = [],
@@ -15,7 +9,17 @@ export default function ProjectItem({
 }) {
   return (
     <div className="project-item">
-      <img className="project-screenshot" src={screenshot} alt={name} />
+      <div className="project-item-image">
+        <div className="project-item-image__overlay">
+          <a href={link} target="_blank">
+            View Project
+          </a>
+          <a href={src} target="_blank">
+            View Code
+          </a>
+        </div>
+        <img className="project-screenshot" src={screenshot} alt={name} />
+      </div>
       <h4 className="project-title">{name}</h4>
       <p className="project-skills">
         {skills.map((skill) => (
